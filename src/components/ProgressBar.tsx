aa264d53f911946af5267703d14fc7af9da4fd99
henriquemaycon <mayconmarquesh@gmail.com>
2023-01-27 20:00:31 -0300

    pre aula 5

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,21 @@
+import * as Progress from "@radix-ui/react-progress";
+
+interface Props {
+   progress: number
+}
+
+export function ProgressBar ({ progress }: Props) {
+   return (
+      <Progress.Root className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+
+         <Progress.Indicator
+            role="progressbar"
+            aria-label="Progresso de hábitos completados nesse dia"
+            aria-valuenow={progress}
+            className="h-3 rounded-xl bg-violet-600"
+            style={{ width: `${progress}%` }}
+         />
+         
+      </Progress.Root>
+   )
+}
